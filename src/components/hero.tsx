da5d1dc168fe37c3365d756974cc00edfa728b9a
harshmangalam <harshdev8218@gmail.com>
2023-08-24 23:33:56 +0530

    change hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -15,9 +15,8 @@ export const Hero = component$(() => (
           <h1>Open source video meetings.</h1>
         </div>
         <p class="py-6 text-lg text-center lg:text-start max-w-xl">
-          An open source and free to use one-to-one private video calling web
-          app build with Qwikcity. Build on top of browser webrtc peer-to-peer
-          communication.
+          An open source one to one video calling web app powered by Qwikcity
+          build on top of browser WebRTC technology.
         </p>
         <div class="grid grid-cols-1 sm:grid-cols-5 gap-4 mt-6 lg:mt-0">
           <CreateRoom />
