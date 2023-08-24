431e974ce7d060df4744826d239d245028db1510
harshmangalam <harshdev8218@gmail.com>
2023-08-24 17:38:32 +0530

    added home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,22 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Navbar } from "~/components/navbar";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <div class="min-h-screen bg-base-100">
+      <Navbar />
+      <HeroSection />
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Qwik meet",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Qwik video meeting web app",
     },
   ],
 };
