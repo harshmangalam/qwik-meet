11a143df0ef8ad3b255899ca0b1998ecb785e815
harshmangalam <harshdev8218@gmail.com>
2023-08-24 17:33:13 +0530

    added navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,20 @@
+import { component$ } from "@builder.io/qwik";
+
+export const Navbar = component$(() => (
+  <nav class="navbar bg-base-200">
+    <div class="flex justify-between flex-1 max-w-7xl mx-auto">
+      <h1 class="text-2xl font-medium">Svelte Meet</h1>
+      <div class="flex items-center">
+        <ThemesDropdown />
+        <a
+          title="Github repo"
+          href="https://github.com/harshmangalam/sveltekit-video-meet"
+          target="_blank"
+          class="btn btn-square btn-ghost"
+        >
+          <iconify-icon width={24} height={24} icon="fa:github" />
+        </a>
+      </div>
+    </div>
+  </nav>
+));
