cc06b61d62ee88a24630199b85d9265104ebf39d
harshmangalam <harshdev8218@gmail.com>
2023-08-24 17:20:18 +0530

    added daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {},
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "halloween",
+      "garden",
+      "forest",
+      "lofi",
+      "fantasy",
+      "luxury",
+      "dracula",
+      "business",
+      "acid",
+      "lemonade",
+    ],
   },
-  plugins: [],
 };
